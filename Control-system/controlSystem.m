e24ce3533c0985025a013c8eab00a9882c423105
Tyaan Singh <singhtyaan@gmail.com>
2017-09-07 15:34:21 +1200

    matlab lead controller design
    Updated matlab lead controller design.

--- Control-system/controlSystem.m
+++ Control-system/controlSystem.m
@@ -1,26 +1,41 @@
 %{
 Model of rocket. Rocket plant has double integral transfer function. 
-Lead controller used. Values of Wb and alpha must be tuned once values 
-of Dm, I and Ft are known. 
+Lead controller used. Max phase lead set at 30 degrees. Frequency of 
+max phase lead set at natural frequency of rocket. 
+
 Currently using arbitrary values of Ft, Dm, I.
 
 
 
 %}
 
-Dm = 0.1;
-I = 0.005;
-Ft = 11;
+Dm = 0.1; %distance from rocket motor to centre of gravity
+I = 0.005; %rotational inertia of the rocket
+Ft = 11; %max motor thrust force
 
-sysOL = tf([Ft*Dm], [I, 0, 0]);
 
-Wb = 8.546;
-a = 1/3;
+sysOL = tf([Ft*Dm], [I, 0, 0]); %Rocket system plant function
 
-C = (1/a)*tf([1, 8.546], [1, 25.638]);
+Wn = sqrt(Ft*Dm/I); %natural frequency of oscillation
 
-sysCL = C*sysOL/(1+C*sysOL);
+alpha = 1/3; %alpha value for lead controller
+Wb = Wn*sqrt(alpha); %Wb value for lead controller
 
+C = (1/alpha)*tf([1, Wb], [1, (Wb/alpha)]); %Lead controller
+
+sysOLC = sysOL*C; %open loop system with controller
+
+sysCL = C*sysOL/(1+C*sysOL); %closed loop system with controller
+
+bode(sysOL);
+title('open loop system');
+
+figure
+bode(sysOLC);
+title('controlled open loop system');
+
+figure
 bode(sysCL);
 [Gm,Pm,Wgm,Wpm] = margin(sysCL);
+title('controller closed loop system');
 
